4e073ce4803a6eaed93814eb31943a77dc8d4914
Renan Rocha <102130210+Renan0Rocha@users.noreply.github.com>
2023-06-09 12:40:24 -0400

    correcao componente footer

--- src/components/patterns/Footer/index.js
+++ src/components/patterns/Footer/index.js
@@ -33,7 +33,7 @@ export default function Footer() {
             color: theme.colors.neutral[400],
           }}
         >
-          © {new Date().getFullYear()} DevRenanRocha. Todos os direitos reservados.
+          © {new Date().getFullYear()} DevRenan0Rocha. Todos os direitos reservados.
         </Text>
       </Box>
     </Box>
